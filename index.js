875377d2fc6ba5fd35a24a1ab80a99f599f2a1ad
Brian <okellobrian18@gmail.com>
2025-02-21 12:50:15 +0300

    Prameters

---
+++ index.js
@@ -0,0 +1,21 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language = JavaScript) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language) {
+    if (!language) {
+        language = "JavaScript"; // Default if no argument is provided
+    }
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+
+
