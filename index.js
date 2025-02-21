07baa0b96455065d081235bc0aed62a524a1331e
Brian <okellobrian18@gmail.com>
2025-02-21 12:51:04 +0300

    Parameters

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ function introductionWithLanguage(name, language) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
 
+function introductionWithLanguageOptional(name, language) {
+    if (!language) {
+        language = "JavaScript"; 
+    }
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
 function introductionWithLanguageOptional(name, language = JavaScript) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
 
-function introductionWithLanguageOptional(name, language) {
-    if (!language) {
-        language = "JavaScript"; // Default if no argument is provided
-    }
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-}
 
 
 
