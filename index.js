295391179174d5665ae4dc97ffa2c52254515d02
Brian <okellobrian18@gmail.com>
2025-02-24 09:29:52 +0300

    slight correction

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ function introductionWithLanguageOptional(name, language) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-function introductionWithLanguageOptional(name, language = JavaScript) {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`
-}
+
 
 
 
